ac72e2e50d65fef18337c27988e9ed50762b49ea
醉翁之意 <Judy_u@163.com>
2021-02-20 13:43:45 +0800

    pod lib create JudyCircularProgressView

---
+++ Example/Pods/Target Support Files/JudyCircularProgressView/JudyCircularProgressView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JudyCircularProgressView : NSObject
+@end
+@implementation PodsDummy_JudyCircularProgressView
+@end
---
+++ Example/Pods/Target Support Files/Pods-JudyCircularProgressView_Tests/Pods-JudyCircularProgressView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JudyCircularProgressView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_JudyCircularProgressView_Tests
+@end
